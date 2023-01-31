2078c0619114ff403661923a334837fda07169a3
dime_coder <karthick.112000@outlook.com>
2023-01-31 17:49:00 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'analyze-data';
+
+  images = ['https://i.pinimg.com/originals/47/0a/19/470a19a36904fe200610cc1f41eb00d9.jpg', 'https://images.unsplash.com/photo-1617396900799-f4ec2b43c7ae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8d2FsbHBhcGVyJTIwNGt8ZW58MHx8MHx8&w=1000&q=80', 'https://newevolutiondesigns.com/images/freebies/cool-4k-wallpaper-3.jpg'];
+currentImageIndex = 0;
+get currentImage() {
+  return this.images[this.currentImageIndex];
+}
+
+  ngOnInit() {
+    setInterval(() => {
+      this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
+    }, 2000);
+  }
+  
 }
