ced0235dd46643e37ee1f9a62d9b8cd704daa52f
dime_coder <karthick.112000@outlook.com>
2023-02-02 12:56:39 +0530

    java commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './Layouts/layout/layout.component';
+import { HomeComponent } from './Components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
